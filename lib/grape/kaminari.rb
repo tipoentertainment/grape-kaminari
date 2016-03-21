67e8c65d4b9dcbe3fb33c4ef5b7beb945f5cbd33
Nick Herro <ncherro@gmail.com>
2016-03-21 14:45:11 -0400

    support pagination with searchkick

--- lib/grape/kaminari.rb
+++ lib/grape/kaminari.rb
@@ -9,14 +9,25 @@ module Grape
       base.class_eval do
         helpers do
           def paginate(collection)
-            collection.page(params[:page]).per(params[:per_page]).padding(params[:offset]).tap do |data|
-              header "X-Total",       data.total_count.to_s
-              header "X-Total-Pages", data.num_pages.to_s
-              header "X-Per-Page",    data.limit_value.to_s
-              header "X-Page",        data.current_page.to_s
-              header "X-Next-Page",   data.next_page.to_s
-              header "X-Prev-Page",   data.prev_page.to_s
+            if collection.is_a?(Searchkick::Results)
+              header "X-Total",       collection.total_count.to_s
+              header "X-Total-Pages", collection.num_pages.to_s
+              header "X-Per-Page",    params[:per_page].to_s
+              header "X-Page",        collection.current_page.to_s
+              header "X-Next-Page",   collection.next_page.to_s
+              header "X-Prev-Page",   collection.prev_page.to_s
               header "X-Offset",      params[:offset].to_s
+              collection
+            else
+              collection.page(params[:page]).per(params[:per_page]).padding(params[:offset]).tap do |data|
+                header "X-Total",       data.total_count.to_s
+                header "X-Total-Pages", data.num_pages.to_s
+                header "X-Per-Page",    data.limit_value.to_s
+                header "X-Page",        data.current_page.to_s
+                header "X-Next-Page",   data.next_page.to_s
+                header "X-Prev-Page",   data.prev_page.to_s
+                header "X-Offset",      params[:offset].to_s
+              end
             end
           end
         end
